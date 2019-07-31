4479e0eb9a202d8b20b9ea02f591041dba319e24
Timothy Graupmann <tim.graupmann@razer.com>
2019-07-30 22:08:02 -0700

    Initial commit of project

---
+++ Source/UE4HTML5SampleSource/UE4HTML5SampleSourceGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4HTML5SampleSourceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4HTML5SAMPLESOURCE_API AUE4HTML5SampleSourceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
